25890bc685e3f188fd9daeac457c39e2d489943d
Gover912G <ngigipaul912@gmail.com>
2025-07-02 22:05:30 +0300

    darkmood effect

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -6,3 +6,23 @@
             navLinks.classList.toggle('show');
         });
     });
+
+    
+  const toggleBtn = document.getElementById('theme-toggle');
+  const userPrefersDark = localStorage.getItem('theme') === 'dark';
+
+  // Set initial theme on page load
+  if (userPrefersDark) {
+    document.body.classList.add('dark-mode');
+    toggleBtn.textContent = '☀️ Light Mode';
+  }
+
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    const isDark = document.body.classList.contains('dark-mode');
+    toggleBtn.textContent = isDark ? '☀️ Light Mode' : '🌙 Dark Mode';
+
+    // Save preference
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  });
